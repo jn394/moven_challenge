5c05e095cca240e18e07ee5e493574ff03ae7d9f
unknown <jayng168@gmail.com>
2019-03-08 11:45:29 -0500

    Added Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # moven_challenge
+
+1) Start by opening the command line and going into the apps directory.
+2) Next type "node app.js" to start the app.
+3) The app wil run and this message will appear "What do you want to do?".
+4) Now the user can type "SET", "GET", "UNSET", "NUMEQUALTO", and "END".
+    - "SET" - Creates the entry with a key and value
+    - "GET" - Shows the value of the specific key
+    - "UNSET" - Removes the key and value of specific key
+    - "NUMEQUALTO" - Shows the number of keys with the specific value
+    - "END" - Ends the appiclation.
+5) After the results of the user input, the message "What do you want to do?" will appear again.
+6) Repeat step 4 
--- app.js
+++ app.js
@@ -1,30 +1,53 @@
+// Requiring the npm inquirer
 var inquirer = require("inquirer");
 
+// Setting up the dictionary
 function Entry() {
     this.data = [];
+    this.nestedData = [];
+    var isBegin = false;
 
+    // The set function
     this.set = function (key, value) {
-        if (key && value) {
-            for (var i = 0; i < this.data.length; i++) {
-                if (this.data[i].key === key) {
-                    this.data[i].value = value;
-                    console.log(this.data);
-                    return start();
+        if (!isBegin) {
+            console.log("yes");
+            if (key && value) {
+                for (var i = 0; i < this.data.length; i++) {
+                    if (this.data[i].key === key) {
+                        this.data[i].value = value;
+                        return start();
+                    }
                 }
+                this.data.push({
+                    key: key,
+                    value: value
+                });
+                start();
             }
-            this.data.push({
-                key: key,
-                value: value
-            });
-            console.log(this.data);
-            start();
         }
+        else {
+            if (key && value) {
+                var newArray = this.nestedData[this.nestedData.length - 1]
+                for (var i = 0; i < newArray.length; i++) {
+                    if (newArray[i].key === key) {
+                        newArray[i].value = value;
+                        return start();
+                    }
+                }
+                newArray.push({
+                    key: key,
+                    value: value
+                });
+                start();
+            }
+        }
+
     };
 
+    // The get function
     this.get = function (key) {
         for (var i = 0; i < this.data.length; i++) {
             if (this.data[i].key === key) {
-                console.log(this.data[i].value);
                 return start();
             }
         }
@@ -32,16 +55,17 @@ function Entry() {
         start();
     };
 
+    // The unset function
     this.unset = function (key) {
         for (var i = 0; i < this.data.length; i++) {
             if (this.data[i].key === key) {
                 this.data.splice(i, 1);
-                console.log(this.data)
             }
         }
         start();
     };
 
+    // The number equal to function 
     this.numequalto = function (value) {
 
         var answer = 0;
@@ -53,17 +77,45 @@ function Entry() {
         }
         console.log(answer);
         start();
-    }
+    };
 
+    // The exit function
     this.exit = function () {
         process.exit();
+    };
+
+    // The begin function doesnt work. I think it has to do with the this.data not being mutable
+    this.begin = function () {
+        if (isBegin) {
+            this.nestedData.push(this.data);
+            start();
+        }
+        else {
+            isBegin = true;
+            var oldData = [];
+            for (var i = 0; i < this.data.length; i++) {
+                oldData.push(this.data[i])
+            }
+            this.nestedData.push(oldData, this.data);
+            start();
+        }
+
+
     }
+
+    // this.print = function () {
+    //     console.log('Old');
+    //     console.log(this.data);
+    //     console.log('Nested');
+    //     console.log(this.nestedData);
+    //     start();
+    // }
 }
 
+// Starting a new dictionary
 var newEntry = new Entry();
 
-
-
+// The start function to set off when the app is run
 function start() {
     inquirer.prompt([
         {
@@ -94,10 +146,17 @@ function start() {
             case "END":
                 newEntry.exit();
                 break;
+            case "BEGIN":
+                newEntry.begin();
+                break;
+            // case "PRINT":
+            //     newEntry.print();
+            //     break;
         };
 
 
     });
 }
 
+// Runs the function 
 start();
